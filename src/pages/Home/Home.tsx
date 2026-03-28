da18bd136dc0fa3d24a052fcb097bb9b641fcb96
fazalhadi26 <fazalhadi1995@gmail.com>
2026-03-28 15:35:51 +0500

    first commit

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,32 +1,21 @@
 import styles from './Home.module.css';
-import bottleImg from '../../assets/bootle1.png';
 import ProductSlider from '../../components/ProductSlider/ProductSlider';
 
 export default function Home() {
   return (
     <div className={styles.homeContainer}>
       <section className={styles.hero}>
-        <div className={styles.bgLayer}></div>
+        <div className={styles.bgLayer} />
 
         <div className={styles.content}>
           <h1 className={styles.title}>
-            <span>PURE WELLNESS,</span>
+            <span>PURE WELLNESS</span>
             <span>NATURALLY REFINED</span>
           </h1>
         </div>
-
-        <div className={styles.bottomLogo}>
-          <svg viewBox="0 0 24 24" fill="white" width="30" height="30">
-            <path d="M12 21L3 4h5.5L12 14.5 15.5 4H21z" />
-          </svg>
-        </div>
-
-        <div className={styles.bottleContainer}>
-          <img src={bottleImg} alt="Vallechiara Spring Water" className={styles.bottleImage} />
-        </div>
       </section>
       
-      {/* Product Slider matches products.png */}
+      {/* Product Slider accurately mapped to layout specifications */}
       <ProductSlider />
     </div>
   );
