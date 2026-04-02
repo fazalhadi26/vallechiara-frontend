b4ba8b99216e8b16ad711eb83c602150d9ab5d0c
fazalhadi26 <fazalhadi1995@gmail.com>
2026-04-02 12:47:55 +0500

    resolve build issue

--- src/pages/Subscribe/Subscribe.tsx
+++ src/pages/Subscribe/Subscribe.tsx
@@ -56,7 +56,7 @@ export default function Subscribe() {
           <div className={styles.step}>
             <div className={styles.stepCircle}>1</div>
             <div className={styles.stepIcon}>
-              <svg width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2" /><line x1="16" y1="2" x2="16" y2="6" /><line x1="8" y1="2" x2="8" y2="6" /><line x1="3" y1="10" x2="21" y2="10" /></svg>
+              <svg width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2" /><line x1="16" y1="2" x2="16" y2="6" /><line x1="8" y1="2" x2="8" y2="6" /><line x1="3" y1="10" x2="21" y2="10" /></svg>
             </div>
             <p>Choose Your Plan</p>
           </div>
@@ -73,7 +73,7 @@ export default function Subscribe() {
           <div className={styles.step}>
             <div className={styles.stepCircle}>3</div>
             <div className={styles.stepIcon}>
-              <svg width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"><rect x="2" y="5" width="20" height="14" rx="2" ry="2" /><line x1="2" y1="10" x2="22" y2="10" /></svg>
+              <svg width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"><rect x="2" y="5" width="20" height="14" rx="2" ry="2" /><line x1="2" y1="10" x2="22" y2="10" /></svg>
             </div>
             <p>Checkout & Enjoy</p>
           </div>
